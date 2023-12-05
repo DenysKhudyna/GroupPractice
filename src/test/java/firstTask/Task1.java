cd6792d2443fdbb1613bdc3fa312139e1ed2f714
dhudy <dhudyna@gmail.com>
2023-12-04 20:52:30 -0600

    First update

---
+++ src/test/java/firstTask/Task1.java
@@ -0,0 +1,14 @@
+package firstTask;
+
+public class Task1 {
+    public String toUpperCase (String str) {
+        return str.toUpperCase();
+    }
+    public String helloWord() {
+        return "Hello World";
+    }
+    public int plus (int firstNum, int secondNum){
+        return firstNum+secondNum;
+    }
+
+}
